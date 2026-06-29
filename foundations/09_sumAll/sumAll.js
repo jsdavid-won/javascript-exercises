12ecb6293f477b094b1220afb23ebe576217bd27
jsdavid-won <jsdavid.won@gmail.com>
2026-06-29 14:20:27 +0900

    Complete JS foundations exercise 9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') return 'ERROR';
+  if (a % 1 !== 0 || b % 1 !== 0) return 'ERROR';
+  if (a < 0 || b < 0) return 'ERROR';
 
+  let small = a;
+  let big = b;
+  if (a > b) {
+    small = b;
+    big = a;
+  }
+
+  let sum = 0;
+  for (let i = small; i < big + 1; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
